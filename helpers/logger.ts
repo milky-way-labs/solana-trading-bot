d7a243ae2400b6377248d19e5a391a52b4403e10
c4rtical <vanzinid98@gmail.com>
2025-04-23 19:20:38 +0200

    aggiornato log orario e aggiunta data

--- helpers/logger.ts
+++ helpers/logger.ts
@@ -1,32 +1,39 @@
+process.env.TZ = 'Europe/Rome';
+
 import pino from 'pino';
+
+const prettyTime = 'SYS:dd/mm/yyyy HH:MM:ss.l "EU/Rome"'; 
 
 const transport = pino.transport({
   targets: [
+
     {
       level: 'trace',
       target: 'pino-pretty',
       options: {
-        destination: "./logs/activity.log",
+        destination: './logs/activity.log',
         colorize: false,
-        colorizeObjects: false,
-        translateTime: "SYS:yyyy-mm-dd HH:MM:ss"
+        translateTime: prettyTime,
       },
     },
+
+    
     {
       level: 'trace',
       target: 'pino-pretty',
-      options: {},
+      options: {
+        colorize: true,
+        translateTime: prettyTime,
+      },
     },
-  ]
+  ],
 });
 
 export const logger = pino(
   {
     level: 'info',
     redact: ['poolKeys'],
-    serializers: {
-      error: pino.stdSerializers.err,
-    },
+    serializers: { error: pino.stdSerializers.err },
     base: undefined,
   },
   transport,
